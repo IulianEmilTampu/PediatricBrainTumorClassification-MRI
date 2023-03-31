0cdf3e6ca9d5b068b0b1ef42ef453d415b5acea1
IulianEmilTampu <iulian.emil.tampu@liu.se>
2023-03-31 07:58:38 +0200

    Adding MIL models

--- models.py
+++ models.py
@@ -1021,3 +1021,177 @@ def age_only_model(
         print(model.summary())
     print(model.summary())
     return model
+
+
+# %% MULTIPLE INSTANCE LEARNING MODEL
+import tensorflow.keras as keras
+import tensorflow.keras.layers as layers
+
+
+class MILAttentionLayer(tf.keras.layers.Layer):
+
+    """Implementation of the attention-based Deep MIL layer.
+
+    Args:
+      weight_params_dim: Positive Integer. Dimension of the weight matrix.
+      kernel_initializer: Initializer for the `kernel` matrix.
+      kernel_regularizer: Regularizer function applied to the `kernel` matrix.
+      use_gated: Boolean, whether or not to use the gated mechanism.
+
+    Returns:
+      List of 2D tensors with BAG_SIZE length.
+      The tensors are the attention scores after softmax with shape `(batch_size, 1)`.
+    """
+
+    def __init__(
+        self,
+        weight_params_dim,
+        kernel_initializer="glorot_uniform",
+        kernel_regularizer=None,
+        use_gated=False,
+        **kwargs,
+    ):
+
+        super().__init__(**kwargs)
+
+        self.weight_params_dim = weight_params_dim
+        self.use_gated = use_gated
+
+        self.kernel_initializer = keras.initializers.get(kernel_initializer)
+        self.kernel_regularizer = keras.regularizers.get(kernel_regularizer)
+
+        self.v_init = self.kernel_initializer
+        self.w_init = self.kernel_initializer
+        self.u_init = self.kernel_initializer
+
+        self.v_regularizer = self.kernel_regularizer
+        self.w_regularizer = self.kernel_regularizer
+        self.u_regularizer = self.kernel_regularizer
+
+    def build(self, input_shape):
+
+        # Input shape.
+        # List of 2D tensors with shape: (batch_size, input_dim).
+        input_dim = input_shape[0][1]
+
+        self.v_weight_params = self.add_weight(
+            shape=(input_dim, self.weight_params_dim),
+            initializer=self.v_init,
+            name="v",
+            regularizer=self.v_regularizer,
+            trainable=True,
+        )
+
+        self.w_weight_params = self.add_weight(
+            shape=(self.weight_params_dim, 1),
+            initializer=self.w_init,
+            name="w",
+            regularizer=self.w_regularizer,
+            trainable=True,
+        )
+
+        if self.use_gated:
+            self.u_weight_params = self.add_weight(
+                shape=(input_dim, self.weight_params_dim),
+                initializer=self.u_init,
+                name="u",
+                regularizer=self.u_regularizer,
+                trainable=True,
+            )
+        else:
+            self.u_weight_params = None
+
+        self.input_built = True
+
+    def call(self, inputs):
+
+        # Assigning variables from the number of inputs.
+        instances = [self.compute_attention_scores(instance) for instance in inputs]
+
+        # Apply softmax over instances such that the output summation is equal to 1.
+        alpha = tf.math.softmax(instances, axis=0)
+
+        return [alpha[i] for i in range(alpha.shape[0])]
+
+    def compute_attention_scores(self, instance):
+
+        # Reserve in-case "gated mechanism" used.
+        original_instance = instance
+
+        # tanh(v*h_k^T)
+        instance = tf.math.tanh(tf.tensordot(instance, self.v_weight_params, axes=1))
+
+        # for learning non-linear relations efficiently.
+        if self.use_gated:
+
+            instance = instance * tf.math.sigmoid(
+                tf.tensordot(original_instance, self.u_weight_params, axes=1)
+            )
+
+        # w^T*(tanh(v*h_k^T)) / w^T*(tanh(v*h_k^T)*sigmoid(u*h_k^T))
+        return tf.tensordot(instance, self.w_weight_params, axes=1)
+
+    def get_config(self):
+
+        config = super().get_config().copy()
+        config.update(
+            {
+                "weight_params_dim": self.weight_params_dim,
+                "kernel_initializer": self.kernel_initializer,
+                "kernel_regularizer": self.kernel_regularizer,
+                "use_gated": self.use_gated,
+            }
+        )
+        return config
+
+
+def MIL_model(
+    num_classes, instance_shape, shared_MIL_encoding_dim: int = 128, bag_size: int = 25
+):
+    denseDropoutRate = 0.5
+    denseRegularizer = "L2"
+    denseConstrain = None
+
+    # Extract features from inputs.
+    inputs, embeddings = [], []
+    shared_dense_layer_1 = layers.Dense(
+        128,
+        activation="relu",
+        kernel_regularizer=denseRegularizer,
+        kernel_constraint=denseConstrain,
+    )
+    shared_dense_layer_2 = layers.Dense(
+        128,
+        activation="relu",
+        kernel_regularizer=denseRegularizer,
+        kernel_constraint=denseConstrain,
+    )
+
+    for _ in range(bag_size):
+        inp = layers.Input(instance_shape)
+        dense_1 = shared_dense_layer_1(inp)
+        dense_2 = shared_dense_layer_2(dense_1)
+        inputs.append(inp)
+        embeddings.append(dense_2)
+
+    # Invoke the attention layer.
+    alpha = MILAttentionLayer(
+        weight_params_dim=shared_MIL_encoding_dim,
+        kernel_regularizer=keras.regularizers.l2(0.01),
+        use_gated=True,
+        name="alpha",
+    )(embeddings)
+
+    # Multiply attention weights with the input layers.
+    multiply_layers = [
+        layers.multiply([alpha[i], embeddings[i]]) for i in range(len(alpha))
+    ]
+
+    # Concatenate layers.
+    concat = layers.concatenate(multiply_layers, axis=1)
+    concat = layers.Dropout(denseDropoutRate)(concat)
+
+    # Classification output node.
+    output = layers.Dense(num_classes, activation="softmax")(concat)
+
+    return keras.Model(inputs, output)

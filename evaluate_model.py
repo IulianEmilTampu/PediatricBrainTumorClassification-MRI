e85a0885be063c9a6e56354c2340e03703018119
IET_ducati <iulian.emil.tampu@liu.se>
2023-11-27 11:52:34 +0100

    Improve plotting and add save of performance to csv file

--- evaluate_model.py
+++ evaluate_model.py
@@ -67,28 +67,70 @@ def check_given_folders(config):
             raise ValueError(f"{fd} not found. Given {folder}.")
 
 
-def set_up(config: dict):
+def set_up(
+    repetition_path,
+    set_to_evaluate: str = "validation",
+    gpu_nbr: int = 0,
+    nbr_workers: int = 15,
+):
+    # load the corrisponding training_configuration .yaml file
+    try:
+        training_config = dict(
+            OmegaConf.load(os.path.join(repetition_path, "hydra_config.yaml"))
+        )
+    except:
+        raise ValueError(
+            f"Training configuration .yaml file not found. Given {repetition_path}"
+        )
+
+    # -----------------------------------------------------------------------------
+    # Build evaluation configuration dictionary based on the training configuration
+    # -----------------------------------------------------------------------------
+    evaluation_config = {
+        "working_folder": repetition_path,
+        "save_path": os.path.join(
+            repetition_path, "evaluation_results", set_to_evaluate
+        ),
+        "model_settings": {
+            "path_to_cross_validation_folder": repetition_path,
+            "model_version": str(
+                os.path.basename(os.path.dirname(Path(repetition_path)))
+            ).split("_")[0],
+            "session_time": os.path.basename(
+                os.path.dirname(Path(repetition_path))
+            ).split("_")[-1],
+            "repetition_nbr": str(os.path.basename(Path(repetition_path))),
+        },
+        "dataloader_settings": {
+            "path_to_data_split_csv_file": os.path.join(
+                repetition_path, "data_split_information.csv"
+            ),
+            "set_to_evaluate": set_to_evaluate,  # is can be training, validation, testing
+            "preprocessing_settings": {
+                "input_size": training_config["dataloader_settings"]["input_size"],
+                "img_mean": training_config["dataloader_settings"]["img_mean"],
+                "img_std": training_config["dataloader_settings"]["img_std"],
+            },
+        },
+        "resources": {"gpu_nbr": gpu_nbr, "nbr_workers": nbr_workers},
+    }
+
+    # ------------------------
+    # Run check on the folder
+    # -----------------------
+    check_given_folders(evaluation_config)
+
     # ---------------------------------------------------------------------------
     # Create folder in the working directory where to save evaluation performance
     # ---------------------------------------------------------------------------
-
-    save_path = os.path.join(
-        config["working_folder"],
-        "validation_results",
-        Path(config["model_settings"]["path_to_cross_validation_folder"]).parents[0],
-        Path(config["model_settings"]["path_to_cross_validation_folder"]).stem,
-        config["dataloader_settings"]["set_to_evaluate"],
-    )
-
-    Path(save_path).mkdir(parents=True, exist_ok=True)
-
-    # save the path in the configuration
-    config["save_path"] = save_path
+    Path(evaluation_config["save_path"]).mkdir(parents=True, exist_ok=True)
 
     # ---------------------
     # Set GPU to be used
     # ---------------------
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(config["resources"]["gpu_nbr"])
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(evaluation_config["resources"]["gpu_nbr"])
+
+    return evaluation_config
 
 
 def run_evaluation(config: dict):
@@ -146,29 +188,16 @@ def run_evaluation(config: dict):
                 files_for_inference = dataset_split
 
 
-# %%  WORKING ON IMPLEMENTATION
-importlib.reload(dataset_utilities)
+# %%  WORKING ON IMPLEMENTATION (support for one model at the time for now)
+model_save_path = "/flush2/iulta54/Code/P5-PediatricBrainTumorClassification_CBTN_v1/trained_model_archive/TESTs_20231124/ResNet50_pretrained_True_frozen_True_0.5_LR_1e-05_BATCH_32_AUGMENTATION_True_OPTIM_adam_SCHEDULER_exponential_MLPNODES_0_t100754"
+# run each repetition independently
+repetitions_to_evaluate = []
+for repetition in glob.glob(os.path.join(model_save_path, "REPETITION_*", "")):
+    repetitions_to_evaluate.append(repetition)
+print(f"Found {len(repetitions_to_evaluate)} Repetitions to work on.")
 
-config = {
-    "working_folder": os.getcwd(),
-    "save_path": None,
-    "dataloader_settings": {
-        "path_to_data_split_csv_file": "/flush2/iulta54/Code/P5-PediatricBrainTumorClassification_CBTN_v1/trained_model_archive/TESTs_20231127/ResNet50_pretrained_True_frozen_True_1.0_LR_1e-05_BATCH_32_AUGMENTATION_False_OPTIM_adam_SCHEDULER_exponential_MLPNODES_0_t082159/REPETITION_1/data_split_information.csv",
-        "set_to_evaluate": "training",  # this can be training, validation, testing
-        "preprocessing_settings": {
-            "input_size": [224, 224],
-            "img_mean": [0.5] * 3,
-            "img_std": [0.5] * 3,
-        },
-    },
-    "model_settings": {
-        "path_to_cross_validation_folder": "/flush2/iulta54/Code/P5-PediatricBrainTumorClassification_CBTN_v1/trained_model_archive/TESTs_20231127/ResNet50_pretrained_True_frozen_True_1.0_LR_1e-05_BATCH_32_AUGMENTATION_False_OPTIM_adam_SCHEDULER_exponential_MLPNODES_0_t082159/REPETITION_1",
-    },
-    "resources": {"gpu_nbr": 0, "nbr_workers": 15},
-}
-
-check_given_folders(config)
-set_up(config)
+config = set_up(repetitions_to_evaluate[0], set_to_evaluate="test")
+# %%  RUN EACH REPETITION
 
 # GET FILES, SET UP LABEL DICTIONARY MAP and the pre-processing transform
 # load .csv file
@@ -231,9 +260,8 @@ for idx, m in enumerate(MODELS):
         f"Fold {idx+1}\n  Last model: {True if m['last'] else None}\n  Best model: {True if m['best'] else None}"
     )
 
-#  %%
-# GET EVALUATION PERFORMANCE ON A SLICE LEVEL
-# here wecan save all the information in a dataframe so that future manipulations are easy to perform.
+#  %% GET EVALUATION PERFORMANCE ON A SLICE LEVEL
+# Here we can save all the information in a dataframe so that future manipulations are easy to perform.
 # The dataframe stores the subject_ID, the class, the file name and the performance for each model in columns MF_1, MF_2, etc.
 # What is saved are the logits (not softmax) scores as outputed from the model for each of the classes.
 summary_evaluation = []
@@ -310,13 +338,14 @@ collapsed_summary_evaluation = collapsed_summary_evaluation.set_index("subject_I
 
 
 # %% WORK ON THE SLICE LEVEL AGGREGATIONS
-# ensemble and get per-slice entropy based uncertainty
 def ensamble_predictions(pd_slice_row, nbr_predictive_models):
-    # this function ensambles the predictions for a single slide in the collased summary evaluation dataframe
+    # this function ensambles the predictions for a single slide over the different folds in the collased summary evaluation dataframe
+
     # get all the predictions for this slice
     predictions = [
         pd_slice_row[f"pred_fold_{i+1}"] for i in range(nbr_predictive_models)
     ]
+
     # remove None
     predictions = [x for x in predictions if x is not None]
     # ensemble predictions
@@ -325,7 +354,8 @@ def ensamble_predictions(pd_slice_row, nbr_predictive_models):
 
 
 def per_slice_per_class_uncertainty(pd_slice_row, nbr_predictive_models):
-    # this function ensambles the predictions for a single slide in the collased summary evaluation dataframe
+    # this function ensambles the predictions for a single slide over the different folds in the collased summary evaluation dataframe
+
     # get all the predictions for this slice
     predictions = [
         pd_slice_row[f"pred_fold_{i+1}"] for i in range(nbr_predictive_models)
@@ -350,9 +380,10 @@ collapsed_summary_evaluation[
     lambda x: per_slice_per_class_uncertainty(x, len(MODELS)), axis=1
 )
 
+
 # %% WORK ON THE PATIENT LEVEL AGGREGATIONS
-
-
+# This is a little bit nasty since, in the case of thraining and validation, there are subjects that are only used in one of the folds.
+# Thus, need to account for the None values and substitute such that the plotting and following code works.
 def proces_subject_wise_predictions(
     grouped_data, func, use_exixting_col_names: bool = True, prefix: str = "new_value"
 ):
@@ -382,8 +413,9 @@ def proces_subject_wise_predictions(
     return grouped_data
 
 
+# ensemble of the predictions for a given slice
 def slice_wise_model_wise_subject_ensemble(x):
-    # here One needs to handrle the cases where there are None values (e.g. subject only use in one fold)
+    # here need to handle the cases where there are None values (e.g. subject only use in one fold)
     aus = []
     for col in x:
         if col.startswith("pred_fold_"):
@@ -395,39 +427,42 @@ def slice_wise_model_wise_subject_ensemble(x):
 
 
 def slice_wise_model_wise_subject_entropy(x):
-    # here One needs to handrle the cases where there are None values (e.g. subject only use in one fold)
+    # here need to handle the cases where there are None values (e.g. subject only use in one fold)
     aus = []
     for col in x:
         if col.startswith("pred_fold_"):
             if x[col][0] != None:
                 aus.append(entropy(softmax(np.array(list(x[col])))).tolist())
             else:
+                # if it is None
                 aus.append([None])
     return aus
 
 
 def slice_wise_subject_ensemble(x):
-    # here One needs to handrle the cases where there are None values (e.g. subject only use in one fold)
+    # here need to handle the cases where there are None values (e.g. subject only use in one fold)
     aus = []
     for col in x:
         if col.startswith("pred_fold_"):
             if x[col][0] != None:
                 aus.append(np.array(list(x[col])).mean(axis=0).tolist())
-            else:
-                aus.append([0, 0, 0])
+            # else:
+            #     # if it is None
+            #     aus.append([None,None,None])
     aus = np.vstack(aus).mean(axis=0).tolist()
     return aus
 
 
 def slice_wise_subject_entropy(x):
-    # here One needs to handrle the cases where there are None values (e.g. subject only use in one fold)
+    # here need to handle the cases where there are None values (e.g. subject only use in one fold)
     aus = []
     for col in x:
         if col.startswith("pred_fold_"):
             if x[col][0] != None:
                 aus.append(np.array(list(x[col])).mean(axis=0).tolist())
-            else:
-                aus.append([0, 0, 0])
+            # else:
+            #     # if it is None
+            #     aus.append([0, 0, 0])
     # compute entropy
     aus = entropy(softmax(np.vstack(aus))).tolist()
     return aus
@@ -458,7 +493,13 @@ collapsed_summary_evaluation = proces_subject_wise_predictions(
     prefix="overall_subject_entropy",
 )
 
-
+# %% INITIATE DICT FOR SUMMARY PERFOMANCE TO CSV
+# this holds the dictionaries from the evaluation_utilities.get_performance_metrics for all the configurations:
+# - per fold slice wise
+# - per slice ensemble across folds
+# - per subject slice wise
+# -per subject across folds and slices ensembles
+summary_performance = []
 # %% PLOT SLICE-WISE RESULTS
 import evaluation_utilities
 
@@ -485,51 +526,57 @@ for col_name in col_names:
     Ptest = np.array([Ptest[i] for i in idx_not_none])
     Ytest_categorical = np.array([Ytest_categorical[i] for i in idx_not_none])
 
-    Ptest_softmax = softmax(Ptest)
+    Ptest_softmax = softmax(np.array(Ptest), axis=1)
+
+    # get metrics
+    aus_dict = evaluation_utilities.get_performance_metrics(
+        Ytest_categorical, Ptest_softmax
+    )
+
+    # add general information
+    aus_dict["model_version"] = "_".join(
+        [
+            config["model_settings"]["model_version"],
+            config["model_settings"]["session_time"],
+        ]
+    )
+    aus_dict["repetition"] = config["model_settings"]["repetition_nbr"]
+    aus_dict["nbr_classes"] = len(list(unique_targe_classes.keys()))
+    aus_dict["classes"] = list(unique_targe_classes.keys())
+    aus_dict["dataset_version"] = "full" if dataset_split.shape[0] > 1500 else "[45,55]"
+    aus_dict["fold_nbr"] = (
+        "all" if col_name == "per_slice_ensemble" else int(col_name.split("_")[-1])
+    )
+    aus_dict["performance_over"] = col_name
+
+    # save
+    summary_performance.append(aus_dict)
 
     # plot metrics
     evaluation_utilities.plotConfusionMatrix(
         GT=Ytest_categorical,
         PRED=Ptest_softmax,
-        classes=["Not_tumor", "Tumor"]
-        if len(one_hot_encodings) == 2
-        else (
-            ["ASTR", "EP", "MED"]
-            if len(one_hot_encodings) == 3
-            else ["ASTR_in", "ASTR_su", "EP_in", "EP_su", "MED_in"]
-        ),
+        classes=list(unique_targe_classes.keys()),
         savePath=config["save_path"],
-        saveName=f"slice_level_CM_{mv}_model_{col_name}",
+        saveName=f"slice_level_CM_{'last'}_model_{col_name}",
         draw=False,
     )
 
     evaluation_utilities.plotROC(
         GT=Ytest_categorical,
         PRED=Ptest_softmax,
-        classes=["Not_tumor", "Tumor"]
-        if len(one_hot_encodings) == 2
-        else (
-            ["ASTR", "EP", "MED"]
-            if len(one_hot_encodings) == 3
-            else ["ASTR_in", "ASTR_su", "EP_in", "EP_su", "MED_in"]
-        ),
+        classes=list(unique_targe_classes.keys()),
         savePath=config["save_path"],
-        saveName=f"slice_level_ROC_{mv}_model_fold_{col_name}",
+        saveName=f"slice_level_ROC_{'last'}_model_fold_{col_name}",
         draw=False,
     )
 
     evaluation_utilities.plotPR(
         GT=Ytest_categorical,
         PRED=Ptest_softmax,
-        classes=["Not_tumor", "Tumor"]
-        if len(one_hot_encodings) == 2
-        else (
-            ["ASTR", "EP", "MED"]
-            if len(one_hot_encodings) == 3
-            else ["ASTR_in", "ASTR_su", "EP_in", "EP_su", "MED_in"]
-        ),
+        classes=list(unique_targe_classes.keys()),
         savePath=config["save_path"],
-        saveName=f"slice_level_PR_{mv}_model_fold_{col_name}",
+        saveName=f"slice_level_PR_{'last'}_model_fold_{col_name}",
         draw=False,
     )
 
@@ -559,60 +606,61 @@ for col_name in col_names:
         ].first()
     )
 
-    idx_not_none = [i for i, _ in enumerate(Ptest) if Ptest[i] != None]
+    idx_not_none = [i for i, _ in enumerate(Ptest) if Ptest[i] != [None]]
     Ptest = np.array([Ptest[i] for i in idx_not_none])
     Ytest_categorical = np.array([Ytest_categorical[i] for i in idx_not_none])
 
-    Ptest_softmax = softmax(Ptest)
-    # Ptest_softmax = softmax(
-    #     list(
-    #         collapsed_summary_evaluation.groupby(collapsed_summary_evaluation.index)[
-    #             col_name
-    #         ].first()
-    #     )
-    # )
+    Ptest_softmax = softmax(np.array(Ptest), axis=1)
+
+    # get metrics
+    aus_dict = evaluation_utilities.get_performance_metrics(
+        Ytest_categorical, Ptest_softmax
+    )
+
+    # add general information
+    aus_dict["model_version"] = "_".join(
+        [
+            config["model_settings"]["model_version"],
+            config["model_settings"]["session_time"],
+        ]
+    )
+    aus_dict["repetition"] = config["model_settings"]["repetition_nbr"]
+    aus_dict["nbr_classes"] = len(list(unique_targe_classes.keys()))
+    aus_dict["classes"] = list(unique_targe_classes.keys())
+    aus_dict["dataset_version"] = "full" if dataset_split.shape[0] > 1500 else "[45,55]"
+    aus_dict["fold_nbr"] = (
+        "all"
+        if col_name == "overall_subject_ensemble"
+        else int(col_name.split("_")[-1])
+    )
+    aus_dict["performance_over"] = col_name
+
+    # save
+    summary_performance.append(aus_dict)
 
     # plot metrics
-    utilities.plotConfusionMatrix(
+    evaluation_utilities.plotConfusionMatrix(
         GT=Ytest_categorical,
         PRED=Ptest_softmax,
-        classes=["Not_tumor", "Tumor"]
-        if len(one_hot_encodings) == 2
-        else (
-            ["ASTR", "EP", "MED"]
-            if len(one_hot_encodings) == 3
-            else ["ASTR_in", "ASTR_su", "EP_in", "EP_su", "MED_in"]
-        ),
+        classes=list(unique_targe_classes.keys()),
         savePath=config["save_path"],
         saveName=f"subject_level_CM_{mv}_model_{col_name}",
         draw=False,
     )
 
-    utilities.plotROC(
+    evaluation_utilities.plotROC(
         GT=Ytest_categorical,
         PRED=Ptest_softmax,
-        classes=["Not_tumor", "Tumor"]
-        if len(one_hot_encodings) == 2
-        else (
-            ["ASTR", "EP", "MED"]
-            if len(one_hot_encodings) == 3
-            else ["ASTR_in", "ASTR_su", "EP_in", "EP_su", "MED_in"]
-        ),
+        classes=list(unique_targe_classes.keys()),
         savePath=config["save_path"],
         saveName=f"subject_level_ROC_{mv}_model_fold_{col_name}",
         draw=False,
     )
 
-    utilities.plotPR(
+    evaluation_utilities.plotPR(
         GT=Ytest_categorical,
         PRED=Ptest_softmax,
-        classes=["Not_tumor", "Tumor"]
-        if len(one_hot_encodings) == 2
-        else (
-            ["ASTR", "EP", "MED"]
-            if len(one_hot_encodings) == 3
-            else ["ASTR_in", "ASTR_su", "EP_in", "EP_su", "MED_in"]
-        ),
+        classes=list(unique_targe_classes.keys()),
         savePath=config["save_path"],
         saveName=f"subject_level_PR_{mv}_model_fold_{col_name}",
         draw=False,
@@ -624,6 +672,10 @@ for col_name in col_names:
 collapsed_summary_evaluation.to_csv(
     os.path.join(config["save_path"], "full_evaluation.csv")
 )
+
+# save also performance
+df = pd.DataFrame(summary_performance)
+df.to_csv(os.path.join(config["save_path"], "summary_performance.csv"))
 
 # %%
 # @hydra.main(version_base=None, config_path="conf", config_name="config")
